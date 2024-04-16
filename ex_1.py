2c5949259cd75f7c389b2b3bf7a1f28579618113
Alo0405 <167082235+Alo0405@users.noreply.github.com>
2024-04-16 12:46:56 +0200

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,9 @@
+name = "valerio"
+age = "19"
+print('my name is {name} and i am {age} years old')
+length = 5
+width = 10
+area = length * width
+perimeter = (length * 2) + (width * 2)
+print(f'the area of the rectangle is: {area}')
+print(f'the perimeter of the rectangle is: {perimeter}')
